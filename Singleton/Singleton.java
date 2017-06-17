e3d8ba4af30569c4bea69a2a5223de662ea1964d
Bhaskar Krishnamurthy <bkbu1024@gmail.com>
2017-06-17 16:53:00 +0530

    singleton dp

---
+++ Singleton/Singleton.java
@@ -0,0 +1,25 @@
+
+public class Singleton {
+	private static Singleton uniqueInstance;
+
+	// other useful instance variables here
+
+	private Singleton() {}
+
+	public static  Singleton getInstance() {
+
+		if (uniqueInstance == null) {
+			synchronized(Singleton.class) {
+				if (uniqueInstance == null) {
+					uniqueInstance = new Singleton();
+				}
+			}
+		}
+		return uniqueInstance;
+	}
+
+	// other useful methods here
+	public String getDescription() {
+		return "I'm a thread safe Singleton!";
+	}
+}
---
+++ Singleton/SingletonClient.java
@@ -0,0 +1,7 @@
+
+public class SingletonClient {
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+		System.out.println(singleton.getDescription());
+	}
+}
